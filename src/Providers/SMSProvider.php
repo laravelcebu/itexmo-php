3ddada268341ad9d8a58a7cea5e35052e84fe94a
Bertrand Kintanar <bertrand@imakintanar.com>
2018-05-24 13:38:52 +0800

    Service Provider changes

--- src/Providers/SMSProvider.php
+++ src/Providers/SMSProvider.php
@@ -2,8 +2,17 @@
 
 namespace LaravelCebu\Itexmo\Providers;
 
+use Illuminate\Support\ServiceProvider;
+use LaravelCebu\Itexmo\Itexmo;
+
 class SMSProvider extends ServiceProvider
 {
+
+    public function provides()
+    {
+        return [Itexmo::class];
+    }
+
     public function boot()
     {
         $this->publishes([
